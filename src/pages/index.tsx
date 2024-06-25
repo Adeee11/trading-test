179d6592ae53b2bd2b0417508dc21555991d1f41
“Adeee11” <hello@iwebcode.design>
2024-06-25 15:33:25 +0530

    fix ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,7 @@ const Index = () => {
   const [newMessage, setNewMessage] = useState("");
 
   useEffect(() => {
-
     socket.on("trade", (message) => {
-
       setMessages(message);
     });
   }, []);
@@ -22,21 +20,23 @@ const Index = () => {
 
   return (
     <div>
-      <div className="grid grid-rows-3 grid-flow-col gap-4">
-        <div className="row-start-1  bg-slate-100 p-28">
-          <h1 className="text-center py-16">YOUR TRADE DETAILS</h1>
-          <div>
+      <div className="grid grid-rows grid-flow-col gap-1 h-svh bg-slate-900 p-28">
+        <div className="row-start-1  bg-slate-100 px-28 pt-8">
+          <h1 className="text-center ">YOUR TRADE DETAILS</h1>
+          <div className="py-8">
             <div>{messages}</div>
           </div>
         </div>
 
-        <div className="row-start-1  bg-slate-100 p-28">
-          <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={sendMessage}
-          >
-            TRADE
-          </button>
+        <div className="row-start-1  bg-slate-100 flex justify-center items-center">
+          <div>
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={sendMessage}
+            >
+              TRADE
+            </button>
+          </div>
         </div>
       </div>
     </div>

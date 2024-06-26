39640eb6750de5101b817c92e73f23d2310bbf2e
“Adeee11” <hello@iwebcode.design>
2024-06-26 18:23:29 +0530

    change backend url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 // const socket = io("http://localhost:3001"); // Replace with your server URL
-const socket = io("http://172.31.35.98:3001/"); 
+const socket = io("https://trading-backend.duckdns.org/"); 
 
 const Index = () => {
   const [error, setError] = useState({ userErr: "", passErr: "" });
